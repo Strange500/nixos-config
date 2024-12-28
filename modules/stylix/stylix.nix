f2acc613cf53730d919a19f5805dde36c9b7c260
Benjamin Roget <benjamin.rogetpro@gmail.com>
2024-12-28 12:37:11 +0100

    change theme

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -6,5 +6,6 @@
       stylix.polarity = "dark";
       stylix.opacity.desktop = 0.5;
       stylix.image = ./wallpaper/apple-dark.jpg;
+      stylix.base16Scheme=  "${pkgs.base16-schemes}/share/themes/codeschool.yaml";
 }
 
