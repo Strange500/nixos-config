589e48de3c81ae239476a27811cbbef1e593febb
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-06-29 08:21:14 +0000

    Clovis second disk config

--- hosts/Clovis/disk-config.nix
+++ hosts/Clovis/disk-config.nix
@@ -1,49 +1,83 @@
-# Example to create a bios compatible gpt partition
+# Example to create a bios compatible gpt partition with second SSD
 { lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      disk1 = {
+        device = lib.mkDefault "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
             };
           };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
+        };
+      };
+
+      # Second SSD - Option 1: Add to existing LVM volume group
+      disk2 = {
+        device = lib.mkDefault "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            lvm = {
+              name = "lvm";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";  # Same volume group as disk1
+              };
             };
           };
         };
       };
     };
+
     lvm_vg = {
       pool = {
         type = "lvm_vg";
         lvs = {
           root = {
-            size = "100%FREE";
+            size = "30%FREE";  # Adjusted to leave space for other volumes
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          # Example additional logical volume using the extra space
+          home = {
+            size = "70%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
               mountOptions = [
                 "defaults"
               ];
