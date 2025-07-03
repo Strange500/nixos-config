adb058106ace07ebbb265bb0b19ea01ce6b59d78
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-07-03 21:48:23 +0200

    update flakes and septimius theme

--- hosts/Septimius/settings.nix
+++ hosts/Septimius/settings.nix
@@ -2,7 +2,7 @@
   config = {
     settings = {
       monitors = [ "DP-1, 1920x1080, 0x0, 1" ];
-      stylix = { theme = "atelier-cave-light"; };
+      stylix = { theme = "atelier-cave"; };
     };
   };
 }
