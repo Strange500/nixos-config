cd1c912ea9705f8c5573edab6c99471fb490fb16
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-07-13 18:42:49 +0200

    enbale Cube auto update

--- hosts/Cube/settings.nix
+++ hosts/Cube/settings.nix
@@ -4,7 +4,7 @@
       desktop = {
         monitors = ["HDMI-A-1, 1920x1080, 0x0, 1" "DP-2, 2560x1440@144, 1920x0, 1"];
       };
-      auto-update = false;
+      auto-update = true;
       apps = {
         sync = {
           desktop.enable = false;
