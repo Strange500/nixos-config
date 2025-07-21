30c2f322450ade3ac6c09e6a91f723e37e61514a
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-07-21 21:08:29 +0200

    added virtiofs mount at boot

--- hosts/Server/configuration.nix
+++ hosts/Server/configuration.nix
@@ -14,6 +14,16 @@
 
   users.mutableUsers = false;
 
+  fileSystems."/mnt/media" = {
+    device = "media";  # Match the tag from libvirt XML
+    fsType = "virtiofs";
+    options = [ 
+      "rw" 
+      "relatime"
+      "user"
+    ];
+  };
+
 
   # firewall
   networking.firewall = {
