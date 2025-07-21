0bbe4535329bd448de82ce51ce356eea3e01af79
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-07-21 20:49:53 +0200

    added jellyfin Secrets

--- hosts/Server/configuration.nix
+++ hosts/Server/configuration.nix
@@ -7,101 +7,13 @@
     ../global.nix
     inputs.home-manager.nixosModules.default
     inputs.sops-nix.nixosModules.sops
+    ../../../modules/server/media/jellyfin.nix
     #../../modules/system/tpm/tpm.nix
     ./disk-config.nix
   ];
 
   users.mutableUsers = false;
 
-  services.declarative-jellyfin = {
-    enable = true;
-    serverId = "68fb5b2c9433451fa16eb7e29139e7f2";
-
-    libraries = {
-      # Movies = {
-      #   enabled = true;
-      #   contentType = "movies";
-      #   pathInfos = [ "/data/Movies" ];
-      #   typeOptions.Movies = {
-      #     metadataFetchers = [
-      #       "The Open Movie Database"
-      #       "TheMovieDb"
-      #     ];
-      #     imageFetchers = [
-      #       "The Open Movie Database"
-      #       "TheMovieDb"
-      #     ];
-      #   };
-      # };
-      # Shows = {
-      #   enabled = true;
-      #   contentType = "tvshows";
-      #   pathInfos = [ "/data/Shows" ];
-      # };
-      # "Photos and videos" = {
-      #   enabled = true;
-      #   contentType = "homevideos";
-      #   pathInfos = [
-      #     "/data/Pictures"
-      #     "/data/Videos"
-      #   ];
-      # };
-      # Books = {
-      #   enabled = true;
-      #   contentType = "books";
-      #   pathInfos = [ "/data/Books" ];
-      # };
-      # Music = {
-      #   enabled = true;
-      #   contentType = "music";
-      #   pathInfos = [ "/data/Music" ];
-      # };
-    };
-
-    users = {
-      Admin = {
-        mutable = false;
-        password = "123";
-        permissions = {
-          isAdministrator = true;
-        };
-      };
-      # Alice = {
-      #   mutable = false;
-      #   hashedPassword = builtins.readFile ../tests/example_hash.txt;
-      #   permissions = {
-      #     isAdministrator = true;
-      #     enableAllFolders = false;
-      #   };
-      #   preferences = {
-      #     # Only allow access to photos and music
-      #     enabledLibraries = [
-      #       "Photos and Videos"
-      #       "Music"
-      #     ];
-      #   };
-      # };
-      # Bob = {
-      #   mutable = false;
-      #   hashedPasswordFile = ../tests/example_hash.txt;
-      #   permissions = {
-      #     isAdministrator = false;
-      #   };
-      # };
-    };
-
-    apikeys = {
-      Jellyseerr = {
-        key = "78878bf9fc654ff78ae332c63de5aeb6";
-      };
-      # Homarr = {
-      #   keyPath = ../tests/example_apikey.txt;
-      # };
-    };
-    openFirewall = true;
-
-    # TODO: add more
-  };
 
   # firewall
   networking.firewall = {
---
+++ modules/server/media/jellyfin.nix
@@ -0,0 +1,102 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  sops = {
+    secrets."server/jellyfin/user/admin/password" = {
+    };
+    secrets."server/jellyfin/user/strange/password" = {
+    };
+  };
+
+  services.declarative-jellyfin = {
+    enable = true;
+    serverId = "68fb5b2c9433451fa16eb7e29139e7f2";
+
+    libraries = {
+      # Movies = {
+      #   enabled = true;
+      #   contentType = "movies";
+      #   pathInfos = [ "/data/Movies" ];
+      #   typeOptions.Movies = {
+      #     metadataFetchers = [
+      #       "The Open Movie Database"
+      #       "TheMovieDb"
+      #     ];
+      #     imageFetchers = [
+      #       "The Open Movie Database"
+      #       "TheMovieDb"
+      #     ];
+      #   };
+      # };
+      # Shows = {
+      #   enabled = true;
+      #   contentType = "tvshows";
+      #   pathInfos = [ "/data/Shows" ];
+      # };
+      # "Photos and videos" = {
+      #   enabled = true;
+      #   contentType = "homevideos";
+      #   pathInfos = [
+      #     "/data/Pictures"
+      #     "/data/Videos"
+      #   ];
+      # };
+      # Books = {
+      #   enabled = true;
+      #   contentType = "books";
+      #   pathInfos = [ "/data/Books" ];
+      # };
+      # Music = {
+      #   enabled = true;
+      #   contentType = "music";
+      #   pathInfos = [ "/data/Music" ];
+      # };
+    };
+
+    users = {
+      Admin = {
+        mutable = false;
+        hashedPasswordFile = config.sops.secrets."server/jellyfin/user/admin/password".path;
+        permissions = {
+          isAdministrator = true;
+        };
+      };
+      Strange = {
+        mutable = true;
+        hashedPasswordFile = config.sops.secrets."server/jellyfin/user/strange/password".path;
+        permissions = {
+          isAdministrator = false;
+          enableAllFolders = false;
+        };
+        preferences = {
+          # Only allow access to photos and music
+          # enabledLibraries = [
+          #   "Photos and Videos"
+          #   "Music"
+          # ];
+        };
+      };
+      Bob = {
+        mutable = false;
+        hashedPasswordFile = ../tests/example_hash.txt;
+        permissions = {
+          isAdministrator = false;
+        };
+      };
+    };
+
+    apikeys = {
+      Jellyseerr = {
+        key = "78878bf9fc654ff78ae332c63de5aeb6";
+      };
+      # Homarr = {
+      #   keyPath = ../tests/example_apikey.txt;
+      # };
+    };
+    openFirewall = true;
+
+    # TODO: add more
+  };
+}
