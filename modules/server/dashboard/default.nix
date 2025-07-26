8b78e73e71c0bfcc838174329c1939246a32700e
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-07-26 09:14:20 +0200

    refactored traefik router & service creation

--- modules/server/dashboard/default.nix
+++ modules/server/dashboard/default.nix
@@ -1,26 +1,8 @@
 {config,...}: {
-  services.traefik.dynamicConfigOptions = {
-    http = {
-      routers = {
-        unraid = {
-          rule = "Host(`unraid.${config.qgroget.server.domain}`)";
-          entryPoints = ["websecure"];
-          service = "unraid";
-          tls = {
-            certResolver = if config.qgroget.server.test.enable then "staging" else "production";
-          };
-        };
-      };
-
-      services = {
-        unraid = {
-          loadBalancer = {
-            servers = [
-              {url = "http://192.168.0.28";}
-            ];
-          };
-        };
-      };
+  traefik.services = {
+    unraid = {
+      name = "unraid";
+      url = "http://192.168.0.28:80";
     };
   };
 }
