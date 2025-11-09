c7efbc043658a19cdd491dd1f71e38f650fa9bb1
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-11-09 14:43:27 +0100

    feat: enhance Jellyfin and Jellyseerr configurations with persistent settings and testing support

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,9 +1,7 @@
-{
-  config,
-  lib,
-  ...
-}: {
+{...}: {
   imports = [
+    ./options.nix
+    ./settings.nix
     ./media
     ./arrs
     ./security
@@ -17,60 +15,4 @@
     ./misc
     ./crypto
   ];
-
-  config = {
-    environment.persistence."/persist".directories = [
-      "${config.qgroget.server.containerDir}"
-      "/var/lib/postgresql"
-    ];
-  };
-
-  options.qgroget = {
-    services = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.submodule {
-        options = {
-          name = lib.mkOption {
-            type = lib.types.str;
-            description = "Service name for subdomain";
-          };
-          url = lib.mkOption {
-            type = lib.types.str;
-            description = "Backend URL";
-          };
-          type = lib.mkOption {
-            type = lib.types.enum ["private" "public"];
-            default = "private";
-            description = "either 'private' or 'public'. 'private' means that the service is only accessible from the local network, while 'public' means it is accessible from the internet.";
-          };
-          middlewares = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [];
-            description = "List of middlewares to apply to the service.";
-          };
-          logPath = lib.mkOption {
-            type = lib.types.str;
-            default = "";
-            description = "Path to the log file for the service.";
-          };
-          journalctl = lib.mkOption {
-            type = lib.types.bool;
-            default = false;
-            description = "If true, the service will be logged using journalctl.";
-          };
-          unitName = lib.mkOption {
-            type = lib.types.str;
-            default = "";
-            description = "The name of the systemd unit for the service, used for journalctl filtering.";
-          };
-          traefikDynamicConfig = lib.mkOption {
-            type = lib.types.attrs;
-            default = {};
-            description = "Additional Traefik dynamic configuration for the service.";
-          };
-        };
-      });
-      default = {};
-      description = "QGroget services to be managed";
-    };
-  };
 }
