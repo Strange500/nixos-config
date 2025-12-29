089f1548e8c6d8709828ea288186af3f5d4b2112
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-12-29 21:37:12 +0100

    test rpi

--- hosts/pi/settings.nix
+++ hosts/pi/settings.nix
@@ -1,32 +1,38 @@
 {lib, ...}: {
   config = {
-    qgroget.nixos = {
-      desktop = {
-        monitors = ["HDMI-A-1, 1920x1080, 0x0, 1" "DP-2, 2560x1440@144, 1920x0, 1"];
-      };
-      auto-update = false;
-      remote-access = {
-        enable = true;
-        tailscale.enable = false;
-        sunshine.enable = false;
-      };
-      apps = {
-        sync = {
-          desktop.enable = false;
-          game.enable = false;
+    qgroget = {
+      # server = {
+      #   domain = "qgroget.com";
+      #   test.enable = false;
+      # };
+      nixos = {
+        auto-update = false;
+        isDesktop = false;
+        theme = "wide";
+        desktop = {
+          desktopEnvironment = lib.mkForce "none";
+          loginManager = lib.mkForce "none";
         };
-        dev = {
-          enable = false;
-          jetbrains.enable = false;
+        remote-access = {
+          enable = true;
+          tailscale.enable = false;
+          sunshine.enable = false;
         };
-        media = false;
-        crypto = false;
+        apps = {
+          basic = false;
+          sync = {
+            desktop.enable = false;
+            game.enable = false;
+          };
+          dev = {
+            enable = false;
+            jetbrains.enable = false;
+          };
+          media = false;
+          crypto = false;
+        };
+        gaming = false;
       };
-      gaming = false;
-      vr = false;
-      desktop.loginManager = lib.mkForce "none";
-      desktop.desktopEnvironment = lib.mkForce "none";
-      settings.bluetooth.enable = false;
     };
   };
 }
