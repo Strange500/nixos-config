a02b83145a7abd45553f0bc86ba38be952bad1d2
Benjamin Roget <benjamin.rogetpro@gmail.com>
2026-03-10 11:26:06 +0100

    notify

--- modules/server/misc/scrutiny.nix
+++ modules/server/misc/scrutiny.nix
@@ -11,16 +11,22 @@
 
   services.scrutiny = {
     enable = true;
-    settings.web.listen.port = 36468;
+    settings = {
+      web.listen.port = 36468;
+
+      notify = {
+        urls = [
+          "generic+https://n8n.qgroget.com/webhook/c8558b6d-c7c2-405d-bfa7-2d59836a5956"
+        ];
+      };
+    };
 
     collector = {
       enable = true;
-      # 2. Hardcode the API endpoint to your custom port
       settings.api.endpoint = "http://127.0.0.1:36468";
     };
   };
 
-  # Custom reverse proxy mapping
   qgroget.services.scrutiny = {
     subdomain = "scrutiny";
     type = "private";
