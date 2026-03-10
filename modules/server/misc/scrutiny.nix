a127828471a11a6ab9570464855e8718c95ca9bd
Benjamin Roget <benjamin.rogetpro@gmail.com>
2026-03-10 10:54:58 +0100

    oups

--- modules/server/misc/scrutiny.nix
+++ modules/server/misc/scrutiny.nix
@@ -1,12 +1,12 @@
 {...}: {
   services.scrutiny = {
     enable = true;
+    settings.web.listen.port = 36468;
   };
 
   qgroget.services.scrutiny = {
     subdomain = "scrutiny";
     type = "private";
     url = "http://127.0.0.1:36468";
-    settings.web.listen.port = 36468;
   };
 }
