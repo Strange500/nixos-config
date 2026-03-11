64be5e418264b8216150ee20d2e931e7ad76b97f
Roget Benjamin <93034371+Strange500@users.noreply.github.com>
2026-03-11 11:27:14 +0100

    feat(scrutiny): use scrutiny fork instead the upstream (#30)
    * feat: update scrutiny configuration to include package override and fetch from GitHub
    
    * test pkgs
    
    * test
    
    * test overlay
    
    * oups
    
    * added system
    
    * correction overlay

--- modules/server/misc/scrutiny.nix
+++ modules/server/misc/scrutiny.nix
@@ -2,7 +2,33 @@
   config,
   lib,
   ...
-}: {
+}: let
+  # Import nixpkgs from the PR #481809 branch
+  # This PR updates scrutiny with new service options and adds scrutiny-collector-zfs
+  scrutinyPkgsSrc = builtins.fetchGit {
+    url = "https://github.com/Samasaur1/nixpkgs";
+    rev = "3d83e50bd8f1336dfc55c627fdf52f96512ef8f6";
+  };
+in {
+  # Use overlay to provide all scrutiny packages from the forked nixpkgs
+  nixpkgs.overlays = [
+    (final: prev: let
+      scrutinyPkgs = import scrutinyPkgsSrc {
+        system = "x86_64-linux";
+      };
+    in {
+      scrutiny = scrutinyPkgs.scrutiny;
+      scrutiny-collector-zfs = scrutinyPkgs.scrutiny-collector-zfs or null;
+    })
+  ];
+
+  # Disable the default scrutiny module and use the one from the forked nixpkgs
+  disabledModules = ["services/monitoring/scrutiny.nix"];
+
+  imports = [
+    "${scrutinyPkgsSrc}/nixos/modules/services/monitoring/scrutiny.nix"
+  ];
+
   services.smartd = {
     enable = true;
     autodetect = true;
