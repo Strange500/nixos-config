796988022cbd7c742ace60028087502ff31b2745
Benjamin Roget <benjamin.rogetpro@gmail.com>
2026-03-11 11:22:51 +0100

    correction overlay

--- modules/server/misc/scrutiny.nix
+++ modules/server/misc/scrutiny.nix
@@ -4,16 +4,24 @@
   ...
 }: let
   # Import nixpkgs from the PR #481809 branch
-  # This PR updates scrutiny with new service options
+  # This PR updates scrutiny with new service options and adds scrutiny-collector-zfs
   scrutinyPkgsSrc = builtins.fetchGit {
     url = "https://github.com/Samasaur1/nixpkgs";
     rev = "3d83e50bd8f1336dfc55c627fdf52f96512ef8f6";
   };
-
-  scrutinyPkgs = import scrutinyPkgsSrc {
-    system = "x86_64-linux";
-  };
 in {
+  # Use overlay to provide all scrutiny packages from the forked nixpkgs
+  nixpkgs.overlays = [
+    (final: prev: let
+      scrutinyPkgs = import scrutinyPkgsSrc {
+        system = "x86_64-linux";
+      };
+    in {
+      scrutiny = scrutinyPkgs.scrutiny;
+      scrutiny-collector-zfs = scrutinyPkgs.scrutiny-collector-zfs or null;
+    })
+  ];
+
   # Disable the default scrutiny module and use the one from the forked nixpkgs
   disabledModules = ["services/monitoring/scrutiny.nix"];
 
@@ -28,7 +36,6 @@ in {
 
   services.scrutiny = {
     enable = true;
-    package = scrutinyPkgs.scrutiny;
     settings = {
       web.listen.port = 36468;
 
