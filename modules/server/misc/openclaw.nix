d3c89eb5088ef86c8a8fcda79b26383951581ec4
Benjamin Roget <benjamin.rogetpro@gmail.com>
2026-08-18 20:05:48 +0200

    feat(openclaw): sandbox openclaw user service to prevent sudo and hide sensitive files

--- modules/server/misc/openclaw.nix
+++ modules/server/misc/openclaw.nix
@@ -197,7 +197,25 @@
       };
     };
 
-    systemd.user.services.openclaw-gateway.Install.WantedBy = ["default.target"];
+    systemd.user.services.openclaw-gateway = {
+      Install.WantedBy = ["default.target"];
+      Service = {
+        # Empêche l'élévation de privilèges (rend sudo/su inopérants)
+        NoNewPrivileges = true;
+        RestrictSUIDSGID = true;
+
+        # Masque complètement les dossiers sensibles et la configuration système
+        InaccessiblePaths = [
+          "-/home/${config.qgroget.user.username}/nixos"
+          "-/home/${config.qgroget.user.username}/.ssh"
+          "-/home/${config.qgroget.user.username}/.gnupg"
+          "-/home/${config.qgroget.user.username}/.local/share/keyrings"
+          "-/run/wrappers/bin/sudo"
+          "-/run/wrappers/bin/su"
+          "-/run/wrappers/bin/doas"
+        ];
+      };
+    };
   };
 
   qgroget.services.openclaw = {
